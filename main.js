e1fee03d6f1adb5c3233d5b7cb1b725d9af4293c
Orashi6 <bekb0323@gmail.com>
2024-11-25 14:51:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let savol1 = prompt('ismingizni kiriting')
+let savol2 = prompt('Space idingizni  kiriting')
+let savol3 = prompt('Space passwordingizni  kiriting')
+
+
+
+
+
+const obj = {
+
+}
+
+
+obj.name = savol1
+obj.SpaceId = savol2
+obj.SpacePassword = savol3
+
+
+
+console.log(obj);
+
